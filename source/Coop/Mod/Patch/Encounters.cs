3e81ff77b4efce026646affd03fe23651ad352b7
Araex <3930554+araex@users.noreply.github.com>
2020-10-04 14:49:58 +0200

    added patch for enter & leave settlement

---
+++ source/Coop/Mod/Patch/Encounters.cs
@@ -0,0 +1,24 @@
+﻿using Sync;
+using TaleWorlds.CampaignSystem;
+
+namespace Coop.Mod.Patch
+{
+    public static class Encounters
+    {
+        // 
+        private static readonly MethodPatch patch = 
+                new MethodPatch(typeof(Campaign))
+                    .Intercept(nameof(Campaign.HandleSettlementEncounter), EMethodPatchFlag.DebounceCalls)
+                    .Intercept(nameof(Campaign.HandlePartyEncounterEvent), EMethodPatchFlag.DebounceCalls)
+            ;
+
+        [PatchInitializer]
+        public static void Init()
+        {
+            CoopClient.Instance.OnPersistenceInitialized += persistence =>
+            {
+                persistence.RpcSyncHandlers.Register(patch.Methods, CoopClient.Instance);
+            };
+        }
+    }
+}
